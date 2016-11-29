fb33a8e54eda9ed63f3d1774c15d69549149508a
Cecil <ccoupe@cableone.net>
2016-11-28 23:55:49 -0700

    move things around, want to separate the GUI from the meat
    * much to be done.

---
+++ ebook-picky.rb
@@ -0,0 +1,8 @@
+# on Linux needs ./search and ./help, otherwise will load Shoes's own files.
+# demo of how to use picky. 
+require 'search_picky'
+require 'help_picky'
+
+Shoes.app(:width => 720, :height => 640, &Shoes.make_help_page("The Shoes eBook"))
+
+# http://www.rubydoc.info/gems/picky
--- ebook.rb
+++ ebook.rb
@@ -1,7 +1,27 @@
-# on Linux needs ./search and ./help, otherwise will load Shoes's own files.
-require 'search_picky'
-require 'help_picky'
-
-Shoes.app(:width => 720, :height => 640, &Shoes.make_help_page("The Shoes eBook"))
-
-# http://www.rubydoc.info/gems/picky
+Shoes.app {
+  stack do
+    flow do
+      button "init ebook" do
+        # create a yaml and .ebook dir
+        dir = ask_open_folder
+        @ebook_dir_el.text = dir;
+        if confirm "make .ebook directory at #{dir}"
+          Dir.mkdir('.ebook') unless Dir.exist?("#{curdir}/.ebook")
+          Dir.mkdir(".ebook/images") unless Dir.exist? "#{curdir}/.ebook/images"
+          # create a bare yaml.
+        end
+      end
+      button "load" do
+        # load the yaml file
+        yf = ask_open_file 
+      end
+      button "render" do
+        # parse the yaml
+        require 'ebook-kd'
+        doc = Kramdown::Document.new(File.read("/home/ccoupe/Projects/shoes3.wiki/chapter-8/Plot-Widget.md")).to_shoes
+        rendering(doc)
+      end
+    end
+    @ebook_dir_el = edit_line width: 400
+  end
+}
